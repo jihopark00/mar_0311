7c41a3e2aefc8d54b41bd087ded246247af3371c
jiho314 <qkrwlgh0314@yonsei.ac.kr>
2026-03-26 20:57:23 +0900

    0326

--- scripts/train_latent.sh
+++ scripts/train_latent.sh
@@ -8,7 +8,7 @@ wandb_project="ssl2gen"
 wandb_entity="qkrwlgh0314"
 
 # Dataset options: cifar10-hf, tiny-imagenet-hf, mnist-hf, imagenet
-run_name="0323_marssllatent_256_cifar10_open8"
+run_name="0326_marssllatent_256_imgnet"
 exps_dir="./ho_mar_0311"
 config=$exps_dir/$run_name/config.yaml
 
@@ -20,7 +20,7 @@ torchrun --nnodes=1 --nproc_per_node=4 --master_port=33221 main_mar_latent.py \
     --config "$config" \
     --dtype "bf16" \
     --num_workers 8 \
-    --eval_freq 10 \
+    --eval_freq 1 \
     --eval_bsz 36 \
     --save_last_freq 1 \
     --num_iter 64 \
